cea5f2e86c3458810aa104015be1496ef3790389
Cody McCarty <codyjmccarty@gmail.com>
2021-06-02 14:35:41 -0500

    2.0

--- classes/school.py
+++ classes/school.py
@@ -6,3 +6,13 @@ class School:
     self.name = name
     self.staff = Staff.objects()
     self.students = Student.objects()
+
+  def list_students(self):
+    count = 0
+    for i,ppl in enumerate(self.students):
+      print (f'{i + 1}. {ppl.name} {ppl.school_id} ')
+
+  def find_student_by_id(self, id):
+    for ppl in self.students:
+      if ppl.school_id == id:
+        print(ppl.name)
